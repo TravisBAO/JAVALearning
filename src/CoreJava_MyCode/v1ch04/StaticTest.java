3759dc65c5a80397eafe052b0ed5221b6e077b3e
TravisBao <travisbao@outlook.com>
2022-08-29 10:26:04 -0400

    practice static method

---
+++ src/CoreJava_MyCode/v1ch04/StaticTest.java
@@ -0,0 +1,65 @@
+package CoreJava_MyCode.v1ch04;
+
+public class StaticTest {
+    public static void main(String[] args) {
+        // fill the staff array with three employee objects
+        var staff = new Employee[3];
+
+        staff[0] = new Employee("Tom", 40000);
+        staff[1] = new Employee("Dick", 60000);
+        staff[2] = new Employee("Harry", 65000);
+
+        for (Employee e : staff)
+        {
+            e.setId();
+            System.out.println("name=" + e.getName() + ",id=" + e.getId()
+             + ",salary=" + e.getSalary());
+        }
+
+        int n = Employee.getNextId();
+        System.out.println("Next available id=" + n);
+    }
+}
+
+class Employee
+{
+    private static int nextId = 1;
+
+    private String name;
+    private double salary;
+    private int id;
+
+    public Employee(String n, double s)
+    {
+        this.name = n;
+        this.salary = s;
+        this.id = 0;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(){
+        id = nextId;
+        nextId++;
+    }
+
+    public static int getNextId(){
+        return nextId;
+    }
+
+    public static void main (String[] args) // unit test
+    {
+        var e = new Employee("Harry", 50000);
+        System.out.println(e.getName() + " " + e.getSalary());
+    }
+}
